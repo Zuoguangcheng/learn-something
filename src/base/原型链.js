069c021efdc61e0ef8abee2d8d4cc960afd4de54
1454252791@qq.com <1454252791@qq.com>
2022-08-29 16:44:38 +0800

    feat: 增加原型链

---
+++ src/base/原型链.js
@@ -0,0 +1,2 @@
+// https://juejin.cn/post/6844903989088092174
+// https://p9-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/d9afcd1172d340508d25c095b1103fac~tplv-k3u1fbpfcp-zoom-in-crop-mark:3024:0:0:0.awebp?
